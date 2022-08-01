cee878682b66f7ecf5b74f8fb056db3ca58f8608
Josh Bennet <joshbennet0@gmail.com>
2022-08-01 16:31:15 +0100

    file and common html added and corrected

--- itemPage.php
+++ itemPage.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>
+        "Races"
+    </title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="keywords" content="HTML, CSS, PHP">
+    <meta name="description" content="Race page to D&D races">
+    <meta name="author" content="Joshua Robert Bennet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
 <?php
+require_once 'functions.php';
+require_once 'database/database.php';
+
+
+?>
+    <h1>
+        "D&D Races"
+    </h1>
+
+
+</body>
+</html>
