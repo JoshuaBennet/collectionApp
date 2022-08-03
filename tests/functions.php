8fcdc94d8d66755439bb2bbcc6bfe23b4239884e
Josh Bennet <joshbennet0@gmail.com>
2022-08-03 14:23:48 +0100

    Started unit testing and make a error net for the function

--- tests/functions.php
+++ tests/functions.php
@@ -1,9 +1,41 @@
 <?php
-
+require 'functions.php';
 
 use PHPUnit\Framework\TestCase;
 
-class testFunctions extends TestCase
+class Functions extends TestCase
 {
+    function testSuccessDisplayRaces()
+    {
+        $input = [];
+        $expectedOutput = '';
+        $actualOutput = createRaceItem($input);
+        $this->assertEquals($expectedOutput, $actualOutput);
+    }
+    function testFailureDisplayRaces()
+    {
+        $input = ['red', 'yellow', 'blue', 'green'];
+        $expectedOutput = '';
+        $actualOutput= createRaceItem($input);
+        $this->assertEquals($expectedOutput, $actualOutput);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
