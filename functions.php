ecc5f65f1327cafd9be79267295f97c19e70dfe1
Josh Bennet <joshbennet0@gmail.com>
2022-08-03 13:45:32 +0100

    updated the database export

--- functions.php
+++ functions.php
@@ -1,29 +1,8 @@
 <?php
 
-//$connectionString = 'mysql:host=db; dbname=dndraces';
-//$dbUsername ='root';
-//$dbPassword = 'password';
-//$db = new PDO($connectionString, $dbUsername, $dbPassword);
-//$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//
-//$queryString = 'SELECT * FROM `races`;';
-//$query = $db->prepare($queryString);
-//$query->execute();
-//
-//$result = $query->fetchAll();
-//
-////echo '<pre>';
-////var_dump($result);
-//
-//    $link_var = '';
-
 function createRaceItem(array $race) {
 
-//    foreach ($race as $trait) {
 
-//        if() {
-//            return '';
-//        }
 
         $string_output = '<div class="race_item">' .
 
@@ -48,34 +27,7 @@ function createRaceItem(array $race) {
             "<p class='text_in_card'>" . $race['otherAddTwo'] . "</p>" .
             "</div>";
 
-//    }
         return $string_output;
-//    $link_var.= $string_output;
 }
-//$some_var = '';
-//foreach (){
 
-
-//
-//            $stage['name'] .
-//            "<p></p>" .
-//            $stage['description'] .
-//            "<p></p>" .
-//            $stage['traits'] .
-//            "<p></p>" .
-//            $stage['variantRaceName'] .
-//            "<p></p>" .
-//            $stage['variantTraits'] .
-//            "<p></p>" .
-////     '<p> <img src="images/' . '$stage['picture']> </p>' .
-//            "</div>" .
-//            '<p class="divider">------------------------------------</p>';
-//            $some_var.=$stringOutput;
-////    }
-//    }
-//}
-
-
-//
-//    var_dump($result);
 
