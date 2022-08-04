2403a6c5bb29f7dadda96b239dd4966c606af3b1
Josh Bennet <joshbennet0@gmail.com>
2022-08-04 12:45:07 +0100

    finished up css for the reg of new races to the site

--- registration.php
+++ registration.php
@@ -34,26 +34,26 @@
 <div class="new_race_container">
     <form class="new_race" method="get" action="submit.php">
 
-        <label for="name" id="name">Name: </label>
-        <input type="text" name="name" alt="input for race name" placeholder="Human" required>
-        <label for="description" id="description">Description: </label>
-        <input type="text" name="description" alt="input for race description" placeholder="A paragraph about the new race" required>
-        <label for="age" id="age">Age: </label>
-        <input type="text" name="age" alt="input for race maximum age" placeholder="Maximum age" required>
-        <label for="size" id="size">Size: </label>
-        <input type="text" name="size" alt="input for race size" placeholder="Description of average size" required>
-        <label  for="speed" id="speed">Speed: </label>
-        <input  type="text" name="speed" alt="input for race speed" placeholder="Your base walking speed is 30 feet" required>
-        <label for="ability" id="ability">Ability increase: </label>
-        <input type="text" name="ability" alt="input for race ability increases" placeholder="Your strength score increases by..." required>
-        <label for="lang" id="lang">Languages known: </label>
-        <input  type="text" name="lang" alt="input for race languages known" placeholder="Languages: You can read, speak, and write...." required>
-        <label for="other" id="other">Other features: </label>
-        <input type="text" name="other" alt="input for other features, not required" placeholder="Additional race features">
-        <label for="otherAdditional" id="otherAdditional">Other features: </label>
-        <input type="text" name="otherAdditional" alt="input for other features, not required" placeholder="Additional race features">
-        <label for="otherAddTwo" id="otherAddTwo">Other features: </label>
-        <input type="text" name="otherAddTwo" alt="input for other features, not required" placeholder="Additional race features">
+        <label class="labels_for_race" for="name" id="name">Name: </label>
+        <input type="text" name="name" alt="input for race name" placeholder="Human" size="40" required>
+        <label class="labels_for_race" for="description" id="description">Description: </label>
+        <input type="text" name="description" alt="input for race description" placeholder="A paragraph about the new race" size="40" required>
+        <label class="labels_for_race" for="age" id="age">Age: </label>
+        <input type="text" name="age" alt="input for race maximum age" placeholder="Maximum age" size="40" required>
+        <label class="labels_for_race" for="size" id="size">Size: </label>
+        <input type="text" name="size" alt="input for race size" placeholder="Description of average size" size="40" required>
+        <label class="labels_for_race" for="speed" id="speed">Speed: </label>
+        <input type="text" name="speed" alt="input for race speed" placeholder="Your base walking speed is 30 feet" size="40" required>
+        <label class="labels_for_race" for="ability" id="ability">Ability increase: </label>
+        <input type="text" name="ability" alt="input for race ability increases" placeholder="Your strength score increases by..." size="40" required>
+        <label class="labels_for_race" for="lang" id="lang">Languages known: </label>
+        <input type="text" name="lang" alt="input for race languages known" placeholder="Languages: You can read,..." size="40" required>
+        <label class="labels_for_race" for="other" id="other">Other features: </label>
+        <input type="text" name="other" alt="input for other features, not required" placeholder="Additional race features" size="40">
+        <label class="labels_for_race" for="otherAdditional" id="otherAdditional">Other features: </label>
+        <input type="text" name="otherAdditional" alt="input for other features, not required" placeholder="Additional race features" size="40">
+        <label class="labels_for_race" for="otherAddTwo" id="otherAddTwo">Other features: </label>
+        <input type="text" name="otherAddTwo" alt="input for other features, not required" placeholder="Additional race features" size="40">
 
         <p></p>
         <input  type="submit"   value="Submit your new race">
